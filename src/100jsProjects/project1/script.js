3f1c64e0905a3370b752d0ba0da0c9446d091c73
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-15 06:57:56 +0400

    progress

--- src/100jsProjects/project1/script.js
+++ src/100jsProjects/project1/script.js
@@ -10,10 +10,32 @@ function updateState() {
 	}
 }
 
-console.log(data[Object.keys(data)[currentState]]);
+function updateDom() {
+	console.log(data[Object.keys(data)[currentState]]);
+	let slider = document.querySelector("#slider");
+	slider.innerHTML = "";
+	let card = document.createElement("div");
+	let image = document.createElement("img");
+	let testimonial = document.createElement("p");
+	let name = document.createElement("p");
+
+	card.className = "card";
+	name.className = "name";
+
+	testimonial.textContent =
+		data[Object.keys(data)[currentState]]["testimonial"];
+	name.textContent = data[Object.keys(data)[currentState]]["name"];
+
+	card.appendChild(image);
+	card.appendChild(testimonial);
+	card.appendChild(name);
+	slider.appendChild(card);
+}
+
+updateDom();
 updateState();
 
 setInterval(() => {
-	console.log(data[Object.keys(data)[currentState]]);
+	updateDom();
 	updateState();
 }, 5000);
