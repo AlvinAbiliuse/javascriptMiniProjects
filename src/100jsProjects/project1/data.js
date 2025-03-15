83cbb950b0bb7086f40f204ec8e039a280749608
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-15 07:01:28 +0400

    data.js added images

--- src/100jsProjects/project1/data.js
+++ src/100jsProjects/project1/data.js
@@ -1,18 +1,18 @@
 let data = {
 	person1: {
-		image: "",
+		image: "./images/person1.jpg",
 		testimonial:
 			"I would also like to say thank you to all your staff. Wow what great service, I love it! Apple impressed me on multiple levels.",
 		name: "Rosetta Q",
 	},
 	person2: {
-		images: "",
+		image: "./images/person2.jpg",
 		testimonial:
 			"This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple.",
 		name: "Cherise G",
 	},
 	person3: {
-		images: "",
+		image: "./images/person3.jpg",
 		testimonial:
 			"Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. The very best.",
 		name: "Constantine V",
