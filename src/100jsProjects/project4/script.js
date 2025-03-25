f8d48b511b6c469496099a99b57df7d07f7189fe
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-25 17:13:39 +0400

    script.js

--- src/100jsProjects/project4/script.js
+++ src/100jsProjects/project4/script.js
@@ -1,1 +1,5 @@
 let api_key = "96d3c6d92ff346dba3e0400a46f3068b";
+
+let tt = fetch("https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=" + api_key);
+
+console.log(tt);
