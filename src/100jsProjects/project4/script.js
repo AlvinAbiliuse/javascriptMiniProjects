3770f3901c6c1619f124ddbfa56e86792a86a82b
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-25 17:17:56 +0400

    progress

--- src/100jsProjects/project4/script.js
+++ src/100jsProjects/project4/script.js
@@ -1,5 +1,6 @@
-let api_key = "96d3c6d92ff346dba3e0400a46f3068b";
 
-let tt = fetch("https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=" + api_key);
+
+let tt = fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+
 
 console.log(tt);
