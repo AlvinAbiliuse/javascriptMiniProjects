e953734869fdc7eb6eec7ad0beb3603fc220f539
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-25 15:51:07 +0400

    script.js

--- src/100jsProjects/project5/script.js
+++ src/100jsProjects/project5/script.js
@@ -1,5 +1,6 @@
 let dice = ["&#9856;", "&#9857;", "&#9858;", "&#9859;", "&#9860;", "&#9861;"];
 
+let n = 1;
 function rollDice() {
 	let current = document.querySelector(".roll");
 	let pastEvents = document.querySelector(".events");
@@ -8,7 +9,7 @@ function rollDice() {
 	let rollNum = document.createElement("p");
 	let diceEvent = document.createElement("p");
 	card.className = "card";
-	rollNum.textContent = "Roll:";
+	rollNum.textContent = `Roll ${n++}:`;
 
 	random = dice[Math.floor(Math.random() * dice.length)];
 
@@ -17,11 +18,10 @@ function rollDice() {
 	card.appendChild(rollNum);
 	card.appendChild(diceEvent);
 
-	pastEvents.appendChild(card);
-
 	current.classList.toggle("wobble");
 	setTimeout(() => {
 		current.innerHTML = random;
+		pastEvents.appendChild(card);
 		current.classList.toggle("wobble");
 	}, 1000);
 }
