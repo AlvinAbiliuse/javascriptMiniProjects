ccdd203e4f8128c43ee742efe23dc58eb1312452
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-25 15:37:10 +0400

    script.js

--- src/100jsProjects/project5/script.js
+++ src/100jsProjects/project5/script.js
@@ -1,7 +1,11 @@
+let dice = ["&#9856;", "&#9857;", "&#9858;", "&#9859;", "&#9860;", "&#9861;"];
+
+function rollDice() {
+	let current = document.querySelector(".roll");
+	current.classList.toggle("wobble");
+	setTimeout(() => current.classList.toggle("wobble"), 1000);
+}
+
 let button = document.querySelector("button");
 
-let current = document.querySelector(".roll");
-
-button.addEventListener("click", () => {
-	current.classList.toggle("wobble");
-});
+button.addEventListener("click", rollDice);
