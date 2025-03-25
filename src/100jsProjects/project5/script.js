1d2b872ab7f30d230b6b03dfde81965c2a46a8bc
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-25 15:43:51 +0400

    script.js

--- src/100jsProjects/project5/script.js
+++ src/100jsProjects/project5/script.js
@@ -3,7 +3,10 @@ let dice = ["&#9856;", "&#9857;", "&#9858;", "&#9859;", "&#9860;", "&#9861;"];
 function rollDice() {
 	let current = document.querySelector(".roll");
 	current.classList.toggle("wobble");
-	setTimeout(() => current.classList.toggle("wobble"), 1000);
+	setTimeout(() => {
+		current.innerHTML = dice[Math.floor(Math.random() * dice.length)];
+		current.classList.toggle("wobble");
+	}, 1000);
 }
 
 let button = document.querySelector("button");
