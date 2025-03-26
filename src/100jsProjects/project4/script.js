d0da9a944c051966f557a374122dae944e21a60c
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-26 15:44:25 +0400

    script.js

--- src/100jsProjects/project4/script.js
+++ src/100jsProjects/project4/script.js
@@ -1,10 +1,13 @@
 import updateDom from "./updateDom.js";
+import dropdown from "./dropdown.js";
 
 async function hello(fet, js) {
 	let req = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
 	let json = await req.json();
 
 	updateDom(json.meals[0]);
+
+	dropdown();
 }
 
 for (let i = 0; i < 10; i++) {
