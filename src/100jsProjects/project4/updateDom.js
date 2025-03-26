29b0f7f8f4e69d2cb9a48ac85e20d235119eeac5
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-26 07:25:01 +0400

    updateDom.js

--- src/100jsProjects/project4/updateDom.js
+++ src/100jsProjects/project4/updateDom.js
@@ -9,8 +9,8 @@ export default function updateDom(data) {
 	let name = document.createElement("h2");
 	let ingredients = document.createElement("p");
 	let linkContainer = document.createElement("div");
-	let yt = document.createElement("button");
-	let recipe = document.createElement("button");
+	let yt = document.createElement("a");
+	let recipe = document.createElement("a");
 	let dropdownContainer = document.createElement("div");
 	let dropdownBtn = document.createElement("button");
 	let instructions = document.createElement("p");
@@ -18,11 +18,18 @@ export default function updateDom(data) {
 	card.className = "card";
 	image.src = data.strMealThumb;
 	name.textContent = data.strMeal;
-	ingredients.textContent = setIngredients(data);
+	ingredients.textContent = "Ingredients: " + setIngredients(data);
+	yt.textContent = "Youtube";
+	recipe.textContent = "Recipe Source";
+	yt.href = data.strYoutube;
+	recipe.href = data.strSource;
 
 	card.appendChild(image);
 	card.appendChild(name);
 	card.appendChild(ingredients);
+	linkContainer.appendChild(yt);
+	linkContainer.appendChild(recipe);
+	card.appendChild(linkContainer);
 
 	container.appendChild(card);
 }
