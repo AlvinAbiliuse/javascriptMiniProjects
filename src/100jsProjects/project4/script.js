56902c981fc9115fbe2ba19f43ba77c46b185ca0
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-26 07:05:17 +0400

    progress

--- src/100jsProjects/project4/script.js
+++ src/100jsProjects/project4/script.js
@@ -5,8 +5,29 @@ async function hello(fet, js) {
 	updateDom(json.meals[0]);
 }
 
+function setIngredients(data) {
+	let n = 2;
+	let instruction = data["strIngredient1"];
+	while (data["strIngredient" + n] != "") {
+		instruction = instruction + "," + data[`strIngredient${n++}`];
+	}
+	console.log(instruction);
+}
 function updateDom(data) {
 	console.log(data);
+	let image = document.createElement("img");
+	let name = document.createElement("h2");
+	let ingredients = document.createElement("p");
+	let linkContainer = document.createElement("div");
+	let yt = document.createElement("button");
+	let recipe = document.createElement("button");
+	let dropdownContainer = document.createElement("div");
+	let dropdownBtn = document.createElement("button");
+	let instructions = document.createElement("p");
+
+	image.src = data.strMealThumb;
+	name.textContent = data.strMeal;
+	ingredients.textContent = setIngredients(data);
 }
 
 for (let i = 0; i < 10; i++) {
