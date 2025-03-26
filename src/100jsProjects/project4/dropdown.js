783534cae6b4a4ef0fba8f13e249a199554d6aa6
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-26 15:55:01 +0400

    dropdown.js

--- src/100jsProjects/project4/dropdown.js
+++ src/100jsProjects/project4/dropdown.js
@@ -2,6 +2,6 @@ export default function dropdown() {
 	let dropper = document.querySelector(".dropdownBtn");
 
 	dropper.addEventListener("click", (e) => {
-		console.log(e.target.parentNode);
+		e.target.parentNode.querySelector(".dropdown").classList.toggle("hidden");
 	});
 }
