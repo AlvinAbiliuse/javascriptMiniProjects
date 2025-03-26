fff7885c3effb86c55f136ff8c8facb7a836ca2f
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-26 07:28:57 +0400

    setIngredients.js

--- src/100jsProjects/project4/setIngredients.js
+++ src/100jsProjects/project4/setIngredients.js
@@ -1,8 +1,13 @@
 export default function setIngredients(data) {
 	let n = 2;
-	let instruction = data["strIngredient1"];
+	let instruction = data["strMeasure1"] + " " + data["strIngredient1"];
 	while (data["strIngredient" + n] != "") {
-		instruction = instruction + ", " + data[`strIngredient${n++}`];
+		instruction =
+			instruction +
+			", " +
+			data[`strMeasure${n}`] +
+			" " +
+			data[`strIngredient${n++}`];
 	}
 	return instruction;
 }
