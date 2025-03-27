a226ea416bbef31f167128e6552c1326f52c2956
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-27 05:23:56 +0400

    progress

--- src/100jsProjects/project4/dropdown.js
+++ src/100jsProjects/project4/dropdown.js
@@ -2,6 +2,8 @@ export default function dropdown() {
 	let dropper = document.querySelector(".dropdownBtn");
 
 	dropper.addEventListener("click", (e) => {
-		e.target.parentNode.querySelector(".dropdown").classList.toggle("hidden");
+		let temp = e.target.parentNode.querySelector(".dropdown");
+		temp.classList.toggle("hidden");
+		console.log("hidden" in temp);
 	});
 }
--- src/100jsProjects/project4/script.js
+++ src/100jsProjects/project4/script.js
@@ -1,11 +1,11 @@
 import updateDom from "./updateDom.js";
 
 async function hello(fet, js) {
-	for (let i = 0; i < 10; i++) {
+	for (let i = 1; i < 11; i++) {
 		let req = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
 		let json = await req.json();
 
-		updateDom(json.meals[0]);
+		updateDom(json.meals[0], i);
 	}
 }
 
