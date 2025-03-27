8a4b741a5c69f1330da6bc07a841496e1f9a3b23
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-27 05:46:31 +0400

    script.js

--- src/100jsProjects/project4/script.js
+++ src/100jsProjects/project4/script.js
@@ -1,6 +1,7 @@
 import updateDom from "./updateDom.js";
+import dropdown from "./dropdown.js";
 
-await fetch("https://dummyjson.com/recipes?limit=10")
+fetch("https://dummyjson.com/recipes?limit=10")
 	.then((res) => res.json())
 	.then((res) => {
 		for (let i in res.recipes) {
