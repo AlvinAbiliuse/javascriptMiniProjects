daf0aefca265a0adeb400bb23fa9806fee42fdf5
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-03-29 07:53:27 +0400

    script.js

--- src/100jsProjects/project4/script.js
+++ src/100jsProjects/project4/script.js
@@ -11,7 +11,13 @@ fetch("https://dummyjson.com/recipes?limit=10")
 		let tt = document.querySelectorAll(".dropdownBtn");
 		for (let i = 0; i < tt.length; i++) {
 			tt[i].addEventListener("click", (e) => {
+				let currentBtn = e.target.parentNode.querySelector(".dropdownBtn");
 				let path = e.target.parentNode.querySelector(".dropdown");
+				if (path.classList.contains("hidden")) {
+					currentBtn.textContent = "Close Full Instructions";
+				} else {
+					currentBtn.textContent = "View Full Instructions";
+				}
 				path.classList.toggle("hidden");
 			});
 		}
