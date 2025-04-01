35bdb8f5a4174d069728c2d9c91ce39f2e1ce129
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-04-01 06:55:33 +0400

    script.js

--- src/100jsProjects/project7/script.js
+++ src/100jsProjects/project7/script.js
@@ -1,27 +1,42 @@
 function playTurn(user) {
-	let comp = ["rock", "paper", "scissors"][Math.random() * 3];
+	let result = document.querySelector(".result");
+	let userScore = document.querySelector(".userScore");
+	let compScore = document.querySelector(".compScore");
+
+	let plays = {
+		rock: { beat: "scissors", lose: "paper" },
+		paper: { beat: "rock", lose: "scissors" },
+		scissors: { beat: "paper", lose: "rock" },
+	};
+
+	let choices = ["rock", "paper", "scissors"];
+
+	let comp = choices[Math.random() * 3];
 	console.log(user, comp);
+	if (plays[user]["beat"] == comp) {
+		console.log(`User Wins! ${user} beats ${comp}`);
+		userScore.textContent = Number(userScore.textContent) + 1;
+	} else if (plays[comp]["beat"] == user) {
+		console.log(`Opponent Wins! ${comp} beats ${user}`);
+		compScore.textContent = Number(compScore.textContent) + 1;
+	}
 }
 
 let button = document.querySelectorAll("button");
 for (let i = 0; i < 3; i++) {
 	button[i].addEventListener("click", (e) => {
-		playTurn(
-			e.target.className != "gameMove"
-				? e.target.className
-				: e.target.parentNode.className
-		);
+		playTurn(e.target.classList[0]);
 	});
 	button[i].addEventListener("mouseover", (e) => {
 		console.log(e);
-		e.target.querySelector("span").classList.add("shake");
+		e.target.classList.add("shake");
 		setTimeout(() => {
-			e.target.querySelector("span").classList.remove("shake");
+			e.target.classList.remove("shake");
 		}, 2000);
 	});
 
 	button[i].addEventListener("mouseout", (e) => {
-		e.target.querySelector("span").classList.remove("shake");
+		e.target.classList.remove("shake");
 	});
 }
 
