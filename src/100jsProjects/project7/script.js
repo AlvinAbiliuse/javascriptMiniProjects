7ba9e56da90007c89e53083b7a034d69d0956fc8
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-04-01 07:01:24 +0400

    completed the script for project7

--- src/100jsProjects/project7/script.js
+++ src/100jsProjects/project7/script.js
@@ -4,20 +4,22 @@ function playTurn(user) {
 	let compScore = document.querySelector(".compScore");
 
 	let plays = {
-		rock: { beat: "scissors", lose: "paper" },
-		paper: { beat: "rock", lose: "scissors" },
-		scissors: { beat: "paper", lose: "rock" },
+		rock: { beat: "scissors" },
+		paper: { beat: "rock" },
+		scissors: { beat: "paper" },
 	};
 
 	let choices = ["rock", "paper", "scissors"];
 
-	let comp = choices[Math.random() * 3];
-	console.log(user, comp);
-	if (plays[user]["beat"] == comp) {
-		console.log(`User Wins! ${user} beats ${comp}`);
+	let comp = choices[Math.floor(Math.random() * 3)];
+
+	if (user == comp) {
+		result.textContent = "Tie!";
+	} else if (plays[user]["beat"] == comp) {
+		result.textContent = `You Win! ${user} beats ${comp}`;
 		userScore.textContent = Number(userScore.textContent) + 1;
 	} else if (plays[comp]["beat"] == user) {
-		console.log(`Opponent Wins! ${comp} beats ${user}`);
+		result.textContent = `Opponent Wins! ${comp} beats ${user}`;
 		compScore.textContent = Number(compScore.textContent) + 1;
 	}
 }
@@ -28,7 +30,6 @@ for (let i = 0; i < 3; i++) {
 		playTurn(e.target.classList[0]);
 	});
 	button[i].addEventListener("mouseover", (e) => {
-		console.log(e);
 		e.target.classList.add("shake");
 		setTimeout(() => {
 			e.target.classList.remove("shake");
@@ -39,26 +40,3 @@ for (let i = 0; i < 3; i++) {
 		e.target.classList.remove("shake");
 	});
 }
-
-/*
-
-let gameMove = document.querySelectorAll(".gameMove");
-for (let i = 0; i < 3; i++) {
-	console.log(gameMove[i]);
-	gameMove[i].addEventListener("mouseover", (e) => {
-		console.log("hi");
-	});
-}
-
-
-
-let rock = document.querySelector(".rock");
-let paper = document.querySelector(".paper");
-let scissors = document.querySelector(".scissors");
-
-
-rock.addEventListener("click", () => console.log("rock"));
-paper.addEventListener("click", () => console.log("paper"));
-scissors.addEventListener("click", () => console.log("scissors"));
-
-*/
