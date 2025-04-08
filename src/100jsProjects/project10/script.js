2b46a406baf9f3c63e844214088e8f65d4735b91
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-04-08 04:27:01 +0400

    script.js

--- src/100jsProjects/project10/script.js
+++ src/100jsProjects/project10/script.js
@@ -1,11 +1,16 @@
 let openBtn = document.querySelector(".openBtn");
 let closeBtn = document.querySelector(".closeBtn");
 let modal = document.querySelector("dialog");
+let video = document.querySelector("video");
 
 openBtn.addEventListener("click", () => {
 	modal.showModal();
+	video.controls = true;
+	video.play();
 });
 
 closeBtn.addEventListener("click", () => {
 	modal.close();
+	video.pause();
+	video.currentTime = 0;
 });
