d9be353dd170721800a0dcce704b0d18bbd7d4aa
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-04-18 07:18:54 +0400

    script.js

--- src/100jsProjects/project012/script.js
+++ src/100jsProjects/project012/script.js
@@ -1,3 +1,15 @@
 import { data } from "./data.js";
 
-console.log(data);
+function updateDom(curr) {
+	document
+		.querySelectorAll("button")
+		.forEach((e) => e.target.classList.remove("current"));
+	curr.classList.add("current");
+	console.log(data[curr.className]);
+}
+
+document.querySelectorAll("button").forEach((e) => {
+	e.addEventListener("click", (curr) => {
+		updateDom(curr.target);
+	});
+});
