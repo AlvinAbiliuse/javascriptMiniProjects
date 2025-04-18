9b8cf5a523c97a2935b425121a415ec279a21bbc
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-04-18 07:36:32 +0400

    script.js

--- src/100jsProjects/project012/script.js
+++ src/100jsProjects/project012/script.js
@@ -2,15 +2,19 @@ import { data } from "./data.js";
 
 function updateDom(curr) {
 	let img = document.querySelector("img");
-	let desc = document.querySelector(".rightLower");
+	let title = document.querySelector(".title");
+	let desc = document.querySelector(".description");
 	document.querySelectorAll("button").forEach((e) => {
 		if (e.className) e.classList.remove("current");
 	});
 	if (curr.className) curr.classList.add("current");
 	let current = data[curr.classList[0]];
 	img.src = current.img;
-	desc = current.description;
+	title.textContent = current.title;
+	desc.textContent = current.description;
 }
+
+updateDom(document.querySelector(".one"));
 
 document.querySelectorAll("button").forEach((e) => {
 	e.addEventListener("click", (curr) => {
