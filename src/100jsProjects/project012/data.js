52c56ad920a3d5a9577304f0232a7bef032f9d7b
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-04-18 05:46:25 +0400

    typo in data.js

--- src/100jsProjects/project012/data.js
+++ src/100jsProjects/project012/data.js
@@ -1,18 +1,24 @@
-	let data = 
-		{one: 
-			{title: "Step 1", 
-			description: "Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
-			img: "./images/one.jpg"}.
-			
-		two: 
-			{title: "Step 2", 
-			description: "Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
-			img: "./images/two.jpg"}.
-			
-		three: 
-			{title: "Step 3", 
-			description: "Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
-			img: "./images/two.jpg"}.
-		}	
+let data = {
+	one: {
+		title: "Step 1",
+		description:
+			"Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
+		img: "./images/one.jpg",
+	},
 
-export data
+	two: {
+		title: "Step 2",
+		description:
+			"Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
+		img: "./images/two.jpg",
+	},
+
+	three: {
+		title: "Step 3",
+		description:
+			"Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
+		img: "./images/two.jpg",
+	},
+};
+
+export { data };
