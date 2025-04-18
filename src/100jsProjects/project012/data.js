6ff544308845f237cab75e0900482c1e7f5801d9
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-04-18 05:40:51 +0400

    data.js

---
+++ src/100jsProjects/project012/data.js
@@ -0,0 +1,18 @@
+	let data = 
+		{one: 
+			{title: "Step 1", 
+			description: "Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
+			img: "./images/one.jpg"}.
+			
+		two: 
+			{title: "Step 2", 
+			description: "Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
+			img: "./images/two.jpg"}.
+			
+		three: 
+			{title: "Step 3", 
+			description: "Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
+			img: "./images/two.jpg"}.
+		}	
+
+export data
