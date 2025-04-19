cd371d669d101ce3056f54e00914dff03fdbf32c
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-04-19 06:01:14 +0400

    data.js

--- src/100jsProjects/project012/data.js
+++ src/100jsProjects/project012/data.js
@@ -2,21 +2,21 @@ let data = {
 	one: {
 		title: "Step 1",
 		description:
-			"Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
+			"Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sint cumque quasi, amet impedit tempore necessitatibus iste. Ratione possimus perspiciatis sapiente, ex alias, ipsam sint beatae vel repellat aperiam corrupti porro et molestiae nobis itaque recusandae vitae atque! Reiciendis voluptatum molestiae autem consectetur corporis libero itaque quo mollitia quod! Corrupti doloribus veniam iusto soluta laborum saepe debitis reiciendis atque nihil eaque ducimus quo ullam ipsum, laboriosam velit vitae sint, eius molestiae unde, tempora hic ipsam? Cupiditate, beatae. Nostrum eos, id velit nisi libero exercitationem, et fugiat assumenda mollitia debitis deleniti, illum iusto. Officiis, ratione ex? Magni, impedit vel. Quam, eaque.",
 		img: "./images/one.jpg",
 	},
 
 	two: {
 		title: "Step 2",
 		description:
-			"Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
+			"Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sint cumque quasi, amet impedit tempore necessitatibus iste. Ratione possimus perspiciatis sapiente, ex alias, ipsam sint beatae vel repellat aperiam corrupti porro et molestiae nobis itaque recusandae vitae atque! Reiciendis voluptatum molestiae autem consectetur corporis libero itaque quo mollitia quod! Corrupti doloribus veniam iusto soluta laborum saepe debitis reiciendis atque nihil eaque ducimus quo ullam ipsum, laboriosam velit vitae sint, eius molestiae unde, tempora hic ipsam? Cupiditate, beatae. Nostrum eos, id velit nisi libero exercitationem, et fugiat assumenda mollitia debitis deleniti, illum iusto. Officiis, ratione ex? Magni, impedit vel. Quam, eaque.",
 		img: "./images/two.jpg",
 	},
 
 	three: {
 		title: "Step 3",
 		description:
-			"Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis",
+			"Consectetur sed necessitatibus sit laudantium adipisci? Aut delectus rem Consectetur nulla fuga consequatur eos dignissimos? Enim itaque itaque veritatis Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sint cumque quasi, amet impedit tempore necessitatibus iste. Ratione possimus perspiciatis sapiente, ex alias, ipsam sint beatae vel repellat aperiam corrupti porro et molestiae nobis itaque recusandae vitae atque! Reiciendis voluptatum molestiae autem consectetur corporis libero itaque quo mollitia quod! Corrupti doloribus veniam iusto soluta laborum saepe debitis reiciendis atque nihil eaque ducimus quo ullam ipsum, laboriosam velit vitae sint, eius molestiae unde, tempora hic ipsam? Cupiditate, beatae. Nostrum eos, id velit nisi libero exercitationem, et fugiat assumenda mollitia debitis deleniti, illum iusto. Officiis, ratione ex? Magni, impedit vel. Quam, eaque.",
 		img: "./images/three.jpg",
 	},
 };
